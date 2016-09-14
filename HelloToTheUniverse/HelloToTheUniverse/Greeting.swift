b1e9b6699e24a9b738d5c064a8f98e9c6e06c4a8
Raymond Lopez <raymondlopez@Raymonds-MacBook-Pro.local>
2016-09-13 23:37:20 -0400

    implement hello universe

--- HelloToTheUniverse/HelloToTheUniverse/Greeting.swift
+++ HelloToTheUniverse/HelloToTheUniverse/Greeting.swift
@@ -16,13 +16,9 @@ class Greeting {
     
     
     
-    // Copy and paste the helloUniverse() function here
-    
     func helloUniverse() -> String {
         return "Hello Universe!"
     }
-    
-    
     
     
     
