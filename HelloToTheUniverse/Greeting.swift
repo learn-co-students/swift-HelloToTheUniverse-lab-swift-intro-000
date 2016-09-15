95327bd71cbd99bf1f85b196358a18f999279e21
jajuki13 <khkim1776@gmail.com>
2016-09-14 23:18:42 -0400

    first function, build failed?

--- HelloToTheUniverse/Greeting.swift
+++ HelloToTheUniverse/Greeting.swift
@@ -12,16 +12,10 @@ import Foundation
 
 class Greeting {
     
-    
-
     // We created the function for you!
     
     func helloUniverse() -> String {
         return "Hello Universe!"
     }
-    
-    
-    
-    
     
 }
