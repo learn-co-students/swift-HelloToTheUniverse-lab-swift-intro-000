200c21371db52024377b9a579967325ed35b6773
Tomasz Jagas <tomaszjagas@MacBookPro.local>
2016-09-16 14:03:13 +0200

    Problem completed

--- HelloToTheUniverseTests/GreetingTest.swift
+++ HelloToTheUniverseTests/GreetingTest.swift
@@ -6,27 +6,27 @@
 //  Copyright © 2016 Flatiron School. All rights reserved.
 //
 
-//import Quick
-//import Nimble
-//@testable import HelloToTheUniverse
-//
-//class GreetingTest: QuickSpec {
-//    override func spec() {
-//        
-//        describe("Greeting Class") {
-//            
-//            let testGreeting = Greeting()
-//            
-//            describe("helloUniverse()") {
-//                it("Should return back the string 'Hello Universe'") {
-//                    
-//                    let actualResponse = testGreeting.helloUniverse()
-//                    let expectedResponse = "Hello Universe!"
-//                    
-//                    expect(actualResponse).to(equal(expectedResponse))
-//                    
-//                }
-//            }
-//        }
-//    }
-//}
+import Quick
+import Nimble
+@testable import HelloToTheUniverse
+
+class GreetingTest: QuickSpec {
+    override func spec() {
+
+        describe("Greeting Class") {
+
+            let testGreeting = Greeting()
+
+            describe("helloUniverse()") {
+                it("Should return back the string 'Hello Universe'") {
+
+                    let actualResponse = testGreeting.helloUniverse()
+                    let expectedResponse = "Hello Universe!"
+
+                    expect(actualResponse).to(equal(expectedResponse))
+
+                }
+            }
+        }
+    }
+}
