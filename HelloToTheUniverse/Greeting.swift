342d899e463d7d0f7606cedb24a111007cba325d
Ezekiel Harpe <ezekielharpe@Zekes-MacBook-Pro-2.local>
2016-10-07 20:39:10 -0400

    Done

--- HelloToTheUniverse/Greeting.swift
+++ HelloToTheUniverse/Greeting.swift
@@ -15,6 +15,9 @@ class Greeting {
     // We created the function for you! Press command + u to run the tests.
     
     func helloUniverse() -> String {        
+        print ("Hello Hello")
+        print("HEY")
+        
         return "Hello Universe!"
     }
     
