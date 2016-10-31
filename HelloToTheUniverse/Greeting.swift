945f400632a66410e7c10877d3c8dfaec33a4183
plate <plate@UP-retina-7.local>
2016-10-31 19:52:41 +0100

    ulriks foerste commit til git

--- HelloToTheUniverse/Greeting.swift
+++ HelloToTheUniverse/Greeting.swift
@@ -14,8 +14,10 @@ class Greeting {
     
     // We created the function for you! Press command + u to run the tests.
     
-    func helloUniverse() -> String {        
+    func helloUniverse() -> String {
+        
         return "Hello Universe!"
+        print ("hej med dig");
     }
     
 }
