1dd37245e3df9c0bbbcb3e22a2e0415049532e09
Rico Talvar <ricotalvar@Ricos-Air.lan>
2017-01-03 22:35:06 +0200

    Complete problems

--- HelloToTheUniverse/Greeting.swift
+++ HelloToTheUniverse/Greeting.swift
@@ -15,6 +15,7 @@ class Greeting {
     // We created the function for you! Press command + u to run the tests.
     
     func helloUniverse() -> String {        
+        print("My name is Rico")
         return "Hello Universe!"
     }
     
