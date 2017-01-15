5f136d0b198f2a923a37839da387289b7d764ed0
Vitor Ferraz <vitorfvarela@gmail.com>
2017-01-15 18:39:36 -0200

    Add print Name

--- HelloToTheUniverse/Greeting.swift
+++ HelloToTheUniverse/Greeting.swift
@@ -14,7 +14,8 @@ class Greeting {
     
     // We created the function for you! Press command + u to run the tests.
     
-    func helloUniverse() -> String {        
+    func helloUniverse() -> String {
+        print("Hello, my name is Vitor and i will become a iOS developer")
         return "Hello Universe!"
     }
     
