f05c2b37a9618249ce7b74402590dfd10b391e0f
Shankar Bellam <srigirishankar@gmail.com>
2017-01-21 17:31:19 -0600

    Complete problems

--- HelloToTheUniverse/Greeting.swift
+++ HelloToTheUniverse/Greeting.swift
@@ -15,7 +15,7 @@ class Greeting {
     // We created the function for you! Press command + u to run the tests.
     
     func helloUniverse() -> String {
-        print("Hey, this is Shankar")
+        print("Hey, this is Shankar yo!")
         
         return "Hello Universe!"
     }
