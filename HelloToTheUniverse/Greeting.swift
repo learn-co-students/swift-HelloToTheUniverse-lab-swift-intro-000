1e0ad7670e12ab2e94d4c0b99ac431af40182963
Tim O'Brien <xxxcodertimxxx@gmail.com>
2017-03-15 08:24:34 -0400

    All tests passing

--- HelloToTheUniverse/Greeting.swift
+++ HelloToTheUniverse/Greeting.swift
@@ -15,7 +15,7 @@ class Greeting {
     // We created the function for you! Press command + u to run the tests.
     
     func helloUniverse() -> String {
-        print("I am going to design some games!")
+        print("Hello Universe!")
         return "Hello Universe!"
     }
     
