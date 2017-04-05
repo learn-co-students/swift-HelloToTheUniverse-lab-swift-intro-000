4a15d83003539c712b4251ae3586fdab2dc58b2d
schrist <SimonChrist@gmx.de>
2017-04-05 13:06:21 +0200

    Passed Tests

--- HelloToTheUniverse/Greeting.swift
+++ HelloToTheUniverse/Greeting.swift
@@ -15,7 +15,7 @@ class Greeting {
     // We created the function for you! Press command + u to run the tests.
     
     func helloUniverse() -> String {        
-        print("Hey there! What's up?")
+        print("Hey there! What's up?!")
         
         return "Hello Universe!"
     }
