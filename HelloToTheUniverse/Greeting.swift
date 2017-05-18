7f4afbb2997d73f2e67f1d4c338c04ea0140bce1
existentializm <e.jaluague@gmail.com>
2017-05-17 17:21:15 -0700

    Answers

--- HelloToTheUniverse/Greeting.swift
+++ HelloToTheUniverse/Greeting.swift
@@ -15,7 +15,7 @@ class Greeting {
     // We created the function for you! Press command + u to run the tests.
     
     func helloUniverse() -> String {        
-        return "Hello Universe!"
+        return "Hello Universe!!"
     }
     
 }
