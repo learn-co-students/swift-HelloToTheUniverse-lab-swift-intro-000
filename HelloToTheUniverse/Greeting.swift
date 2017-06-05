8efc415adc78498bcff4880c7a1551bb87cee86e
hushangni <shangnihu@gmail.com>
2017-06-04 23:19:11 -0400

    Changed print statement

--- HelloToTheUniverse/Greeting.swift
+++ HelloToTheUniverse/Greeting.swift
@@ -15,7 +15,7 @@ class Greeting {
     // We created the function for you! Press command + u to run the tests.
     
     func helloUniverse() -> String {
-        print "Hello! To everyone watching the swift video."
+        print("Hello! To everyone watching the swift video.")
         return "Hello Universe!"
     }
     
