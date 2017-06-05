7cecf39b64d64cc998759dd35d9925f367290baa
XavierVivar <xavier.vivar@gmail.com>
2017-06-05 16:30:54 -0400

    Adding printing statement

--- HelloToTheUniverse/Greeting.swift
+++ HelloToTheUniverse/Greeting.swift
@@ -14,9 +14,10 @@ class Greeting {
     
     // We created the function for you! Press command + u to run the tests.
     
-    func helloUniverse() -> String {        
+    func helloUniverse() -> String {
+        print("This is not working")
         return "Hello Universe!"
     }
     
 }
-   
+
