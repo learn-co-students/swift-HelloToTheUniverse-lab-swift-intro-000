9e755ee2b79b86f38797820d7d3020884b5953c0
sercosan <serconde@icloud.com>
2017-06-12 12:04:07 -0400

    Complete problems

--- HelloToTheUniverse/Greeting.swift
+++ HelloToTheUniverse/Greeting.swift
@@ -14,7 +14,9 @@ class Greeting {
     
     // We created the function for you! Press command + u to run the tests.
     
-    func helloUniverse() -> String {        
+    func helloUniverse() -> String {
+        print("I believe I can fly")
+        
         return "Hello Universe!"
     }
     
