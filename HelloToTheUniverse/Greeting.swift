0c354663fe9c98c175eea23e41f66a9e3db72560
kev0587 <kev.0587@gmail.com>
2017-06-22 08:57:51 -0500

    ya

--- HelloToTheUniverse/Greeting.swift
+++ HelloToTheUniverse/Greeting.swift
@@ -15,7 +15,7 @@ class Greeting {
     // We created the function for you! Press command + u to run the tests.
     
     func helloUniverse() -> String {
-        print("hey kevin")
+        print("sup")
         return "Hello Universe!"
     }
     
