9db53695e70794409a72f5afbfb9f5d25f83ac7c
Andrey Gatitskiy <gatitskiy@gmail.com>
2017-08-12 10:42:51 +0300

    Pull

--- HelloToTheUniverse/Greeting.swift
+++ HelloToTheUniverse/Greeting.swift
@@ -13,8 +13,8 @@ import Foundation
 class Greeting {
     
     // We created the function for you! Press command + u to run the tests.
-    
-    func helloUniverse() -> String {        
+    func helloUniverse() -> String {
+        print("Pull me!")
         return "Hello Universe!"
     }
     
