23552a741e7e991892b6fd9badbfb21e64ca6973
marcusabiona <marcusabiona@hotmail.com>
2017-08-25 20:24:43 +0100

    Another Complete Test

--- HelloToTheUniverse/Greeting.swift
+++ HelloToTheUniverse/Greeting.swift
@@ -14,7 +14,9 @@ class Greeting {
     
     // We created the function for you! Press command + u to run the tests.
     
-    func helloUniverse() -> String {        
+    func helloUniverse() -> String {
+        print("The correct phrase is Hello World")
         return "Hello Universe!"
-        print "The correct phrase is Hello World" } }
+    }
+}
    
