7156185b7858ea4e59b4d058cdf5a9660f544e29
esmeraldaesk <maggie.jaglo@gmail.com>
2017-09-05 21:29:01 +0200

    Complete problems

--- HelloToTheUniverse/Greeting.swift
+++ HelloToTheUniverse/Greeting.swift
@@ -6,17 +6,3 @@
 //  Copyright © 2016 Flatiron School. All rights reserved.
 //
 
-import Foundation
-
-
-
-class Greeting {
-    
-    // We created the function for you! Press command + u to run the tests.
-    
-    func helloUniverse() -> String {        
-        return "Hello Universe!"
-    }
-    
-}
-   
