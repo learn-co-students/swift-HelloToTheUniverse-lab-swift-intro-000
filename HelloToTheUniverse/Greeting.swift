1fb0a2556fae9f0b87b49845bc6d8a498d60005e
brahim1505 <brahim.abdellatif@yahoo.com>
2017-09-08 01:05:12 +0200

    Add print statement

--- HelloToTheUniverse/Greeting.swift
+++ HelloToTheUniverse/Greeting.swift
@@ -15,8 +15,7 @@ class Greeting {
     // We created the function for you! Press command + u to run the tests.
     
     func helloUniverse() -> String {
-        print("Hello everyone watching this.")
-        
+        print("hello everyone")
         return "Hello Universe!"
     }
     
