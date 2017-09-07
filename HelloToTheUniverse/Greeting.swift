3b004d5430f61c3a154816dbf633d7ca4a4cc711
Qin WAN <rogerwq@gmail.com>
2017-09-07 13:16:11 +0800

    Complete problems

--- HelloToTheUniverse/Greeting.swift
+++ HelloToTheUniverse/Greeting.swift
@@ -15,7 +15,7 @@ class Greeting {
     // We created the function for you! Press command + u to run the tests.
     
     func helloUniverse() -> String {
-        print("Hello Roger")
+        print("My name is Buzz and I can fly")
         
         return "Hello Universe!"
     }
