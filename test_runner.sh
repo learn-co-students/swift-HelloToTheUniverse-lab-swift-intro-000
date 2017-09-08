cb8dbe57abc6d02ae3f5d0012e31ca1b153060ca
brahim1505 <brahim.abdellatif@yahoo.com>
2017-09-08 19:13:41 +0200

    Complete problems

--- test_runner.sh
+++ test_runner.sh
@@ -24,7 +24,7 @@ TOTAL_COUNT=`tail -n10 unixfile.txt | grep -A1 xctest | grep Executed | awk '{pr
 FAILURE_COUNT=`tail -n10 unixfile.txt | grep -A1 xctest  | grep Executed | awk '{print $5}'`
 PASSING_COUNT=`echo "${TOTAL_COUNT} - ${FAILURE_COUNT}" | bc`
 REPO_NAME=`echo ${CURR_DIR} | awk -F'/' '{print $NF}'`
-cd ${CURR_DIR}
+
 
 
 curl -s -H "Content-Type: application/json" -X POST --data "{ \"username\": \"${GITHUB_USERNAME}\", \"github_user_id\": \"${GITHUB_USER_ID}\", \"repo_name\": \"${REPO_NAME}\", \"build\": { \"test_suite\": [{\"framework\": \"xcpretty\", \"formatted_output\": [], \"duration\": 0.0, \"build_output\": []}]}, \"total_count\": ${TOTAL_COUNT}, \"passing_count\": ${PASSING_COUNT}, \"failure_count\": ${FAILURE_COUNT}}" "${SERVICE_URL}${SERVICE_ENDPOINT}"
