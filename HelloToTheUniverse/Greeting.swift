e427271ac2c78f1141f39648f1dbf48992425b8d
timothehippo <tyxhul@yahoo.com>
2017-10-21 15:01:02 -0400

    pull test

--- HelloToTheUniverse/Greeting.swift
+++ HelloToTheUniverse/Greeting.swift
@@ -15,7 +15,7 @@ class Greeting {
     // We created the function for you! Press command + u to run the tests.
     
     func helloUniverse() -> String {
-        print("testing of xcode")
+        print("testing")
         return "Hello Universe!"
     }
     
