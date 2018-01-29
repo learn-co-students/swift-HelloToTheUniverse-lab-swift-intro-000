62fe0331e04aa9c6fb719455141c7a886317e60b
chenjm1960 <chenjm1960@yahoo.com>
2018-01-29 14:24:57 -0800

    completed step 1

--- HelloToTheUniverse/Greeting.swift
+++ HelloToTheUniverse/Greeting.swift
@@ -14,7 +14,8 @@ class Greeting {
     
     // We created the function for you! Press command + u to run the tests.
     
-    func helloUniverse() -> String {        
+    func helloUniverse() -> String {
+        print("adding new line as test")
         return "Hello Universe!"
     }
     
