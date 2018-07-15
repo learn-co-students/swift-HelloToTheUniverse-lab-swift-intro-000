c984eb6c1193d62c25b208d493bf2d39e3f660ff
Yousef Albanay <yalbannaii@users.noreply.github.com>
2018-07-15 09:10:45 -0500

    Complete problems

--- HelloToTheUniverse/Greeting.swift
+++ HelloToTheUniverse/Greeting.swift
@@ -14,7 +14,8 @@ class Greeting {
     
     // We created the function for you! Press command + u to run the tests.
     
-    func helloUniverse() -> String {        
+    func helloUniverse() -> String {
+        print("My Name is Yousef")
         return "Hello Universe!"
     }
     
