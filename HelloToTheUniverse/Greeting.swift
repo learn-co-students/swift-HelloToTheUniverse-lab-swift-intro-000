d7d73a933be030383cf043ef12452b32c3111bbe
chadarutherford <bladz82@hotmail.com>
2018-11-08 14:01:57 -0500

    Complete Problems

--- HelloToTheUniverse/Greeting.swift
+++ HelloToTheUniverse/Greeting.swift
@@ -14,7 +14,8 @@ class Greeting {
     
     // We created the function for you! Press command + u to run the tests.
     
-    func helloUniverse() -> String {        
+    func helloUniverse() -> String {
+        print("My name is Chad and I want to be a developer!")
         return "Hello Universe!"
     }
     
